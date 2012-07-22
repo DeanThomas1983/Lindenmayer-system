fcb44801388e5930f6d9c7d9d7a3141736d40bd7
DeanThomas1983 <dean.plymouth@googlemail.com>
2012-07-22 17:00:39 +0100

    Begin to add support for English chars and String in "LetterEditForm".

--- LindenmayerSystemJava/src/lindenmayersystemjava/Lindenmayer.java
+++ LindenmayerSystemJava/src/lindenmayersystemjava/Lindenmayer.java
@@ -5,11 +5,7 @@
 package lindenmayersystemjava;
 
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Image;
 import java.awt.geom.Point2D;
-import java.awt.image.ImageObserver;
-import java.awt.image.ImageProducer;
 import java.util.ArrayList;
 import sun.security.util.Debug;
 
